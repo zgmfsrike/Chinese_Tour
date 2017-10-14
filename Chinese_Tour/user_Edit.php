fe028fa0a673c27dc34d325a5bc65e208e7d8543
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2017-10-14 19:00:30 +0700

    Update User_Edit
    add check session and declare new variable $username for check who is in the system for edit the information

--- Chinese_Tour/user_Edit.php
+++ Chinese_Tour/user_Edit.php
@@ -1,10 +1,14 @@
 <?php
 //----------------------------Wait for session---------------------------------------------//
-
+session_start();
 
 
 //-----------------------------Variable----------------------------------------------------//
 $host = mysqli_connect('localhost', 'root', '7744536','testlogin');
+// include "database/db_config.php";
+// include "module/hashing.php";
+if(isset($_POST['username'])){
+$username = $_POST['username'];
 $firstname = $_POST['firstname'];
 $middlename = $_POST['middlename'];
 $surname = $_POST['surname'];
@@ -20,7 +24,7 @@ $email = $_POST['emailid'];
 
 //-----------------------------Edit fucntion----------------------------------------------------//
 $sql= "UPDATE `member` SET `first_name`='$firstname', `middle_name`='$middlename',`last_name`='$surname',`address`='$address',
-                            `phone`='$phone',`occupation`='$occupation',`salary`='$salary',`dob`='$time',`email`='$email' WHERE id = 3 ";
+                            `phone`='$phone',`occupation`='$occupation',`salary`='$salary',`dob`='$time',`email`='$email' WHERE username = $username  ";
 
 
 //-----------------------------Send change mail fucntion----------------------------------------------------//
@@ -34,6 +38,10 @@ if($query){
   echo "Record update successfully";
 }
 
+}else{
+    $error = "Your Login Name or Password is invalid";
+    header("location: login.html");
+}
 
 
 ?>
