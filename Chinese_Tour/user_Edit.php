99e0a20d23d9bc536bc46ee3acceef9cf1fd52a0
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2017-10-14 19:36:47 +0700

    Add demo send email
    add chceck session and demo send email function

--- Chinese_Tour/user_Edit.php
+++ Chinese_Tour/user_Edit.php
@@ -7,8 +7,9 @@ session_start();
 $host = mysqli_connect('localhost', 'root', '7744536','testlogin');
 // include "database/db_config.php";
 // include "module/hashing.php";
-if(isset($_POST['username'])){
-$username = $_POST['username'];
+if(isset($_SESSION['username'])){
+$username = $_SESSION['username'];
+
 $firstname = $_POST['firstname'];
 $middlename = $_POST['middlename'];
 $surname = $_POST['surname'];
@@ -28,7 +29,34 @@ $sql= "UPDATE `member` SET `first_name`='$firstname', `middle_name`='$middlename
 
 
 //-----------------------------Send change mail fucntion----------------------------------------------------//
+// send e-mail to ...
+$to=$email;
 
+// Your subject
+$subject="Your address email is changed";
+
+// Your message
+$message="Your Comfirmation link \r\n";
+$message.="Click on this link to activate your account \r\n";
+$message.="http://www.yourweb.com/confirmation.php?passkey=$confirm_code";
+
+// send email
+$sentmail = mail($to,$subject,$message);
+}
+
+// if not found
+else {
+echo "Not found your email in our database";
+}
+
+// if your email succesfully sent
+if($sentmail){
+echo "Your Confirmation link Has Been Sent To Your New Email Address.";
+}
+else {
+echo "Cannot send Confirmation link to your e-mail address";
+}
+?>
 
 
 
