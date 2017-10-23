2b58d98b324cab6fae99a174cf1ce98296e66d58
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2017-10-23 22:35:01 +0700

    update Edit_info eiei

--- Chinese_Tour/user_Edit.php
+++ Chinese_Tour/user_Edit.php
@@ -1,10 +1,14 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 //----------------------------Wait for session---------------------------------------------//
 session_start();
 
 
 //-----------------------------Variable----------------------------------------------------//
 include "db_config.php";
+// include "db_configNB.php";
 include "module/hashing.php";
 if(isset($_SESSION['login_id'])){
 $id = $_SESSION['login_id'];
@@ -28,34 +32,47 @@ $sql= "UPDATE `member` SET `first_name`='$firstname', `middle_name`='$middlename
 
 
 //-----------------------------Send change mail fucntion----------------------------------------------------//
-// send e-mail to ...
-$to=$email;
+require 'vendor/autoload.php';
+// $url = "http://localhost/Chinese_Tour/Chinese_Tour/home.php"
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = "zgmfsrike@gmail.com";                 // SMTP username
+    $mail->Password = 'amenoera7744';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
 
-// Your subject
-$subject="Your address email is changed";
+    //Recipients
+    $mail->setFrom('info@chtour.com', 'Chinese Tour');
+    $mail->addAddress($email);
+    // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
-// Your message
-$message="Your Comfirmation link \r\n";
-$message.="Click on this link to activate your account \r\n";
-$message.="http://www.yourweb.com/confirmation.php?passkey=$confirm_code";
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-// send email
-$sentmail = mail($to,$subject,$message);
+    //Content
+    $body = '<p><strong>The user email is changed , Click the link below to go to homepage</strong><br>
+        Link : '.$url.'</p>';
+    $mail->isHTML(true);                                  // Set email format to HTML
+     $mail->Subject = 'Chinese Tour: Email address is changed';
+    $mail->Body    = $body;
+    $mail->AltBody = strip_tags($body);
+
+    $mail->send();
+    // echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
-
-// if not found
-else {
-echo "Not found your email in our database";
-}
-
-// if your email succesfully sent
-if($sentmail){
-echo "Your Confirmation link Has Been Sent To Your New Email Address.";
-}
-else {
-echo "Cannot send Confirmation link to your e-mail address";
-}
-
 
 
 
