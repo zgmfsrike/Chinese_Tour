4f7b6732ddc21b7b9b454e46d7fc7aae67d68f62
ploychanok <plo_y@live.com>
2017-11-01 23:10:26 +0700

    add contact container

--- Chinese_Tour/about.php
+++ Chinese_Tour/about.php
@@ -82,7 +82,35 @@
         </div>
       </div>
       <!-- /.row -->
-
+      <!-- Content Row -->
+            <div class="row">
+              <!-- Map Column -->
+              <div class="col-lg-8 mb-4">
+                <!-- Embedded Google Map -->
+                <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+              </div>
+              <!-- Contact Details Column -->
+              <div class="col-lg-4 mb-4">
+                <h3>Contact Details</h3>
+                <p>
+                  3481 Melrose Place
+                  <br>Beverly Hills, CA 90210
+                  <br>
+                </p>
+                <p>
+                  <label title="Phone">Tel</lang="">: (123) 456-7890
+                </p>
+                <p>
+                  <label title="Email">Email</label>:
+                  <a href="mailto:name@example.com">name@example.com
+                  </a>
+                </p>
+                <p>
+                  <label title="Hours">Office Hour</label><br> Monday - Friday 9:00 AM to 5:00 PM
+                </p>
+              </div>
+            </div>
+            <!-- /.row -->
     </div>
     <!-- /.container -->
 
