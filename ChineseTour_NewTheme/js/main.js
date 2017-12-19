0f6b96667408f5b41e5af53a199d27951db6690b
ploychanok <plo_y@live.com>
2017-12-20 04:17:04 +0700

    NewTheme
    แก้กันให้บึ้มไปเรย++
    //ฝากแปะ partial view header กับ footer ด้วย

---
+++ ChineseTour_NewTheme/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+     $('.slider').slider({full_width: true});
+     $('.tooltipped').tooltip();
+
+     $('.datepicker').pickadate({
+       selectMonths: true, // Creates a dropdown to control month
+       selectYears: 50, // Creates a dropdown of 15 years to control year,
+       today: 'Today',
+       clear: 'Clear',
+       close: 'Ok',
+       closeOnSelect: false, // Close upon selecting a date,
+       max: 'Today'
+    });
+    $('#datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 50, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false, // Close upon selecting a date,
+      min: 'Today'
+   });
+   $('#datepicker2').pickadate({
+     selectMonths: true, // Creates a dropdown to control month
+     selectYears: 50, // Creates a dropdown of 15 years to control year,
+     today: 'Today',
+     clear: 'Clear',
+     close: 'Ok',
+     closeOnSelect: false, // Close upon selecting a date,
+     min: 'Today'
+  });
+
+    $('select').material_select();
+    $(".button-collapse").sideNav();
+    $('.chips-initial').material_chip('data');
+});
