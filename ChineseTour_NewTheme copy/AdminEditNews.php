43063ada9ab5f4ec665f9661f77755ec1992978e
ploychanok <plo_y@live.com>
2017-12-26 16:31:23 +0700

    addFile

--- ChineseTour_NewTheme copy/AdminEditNews.php
+++ ChineseTour_NewTheme copy/AdminEditNews.php
@@ -2,7 +2,7 @@
   <html>
   <?php
   include('module/session.php');
-  isAdmin();
+  isLogin();
    ?>
    <?php
       include 'component/adminHeader.php';
@@ -25,62 +25,40 @@
               </div>
               <div class="row">
                 <h5>Add Images</h5>
-                <div class="col s12 l6">
-                  <form action="#">
+                <div class="col s12">
+                  <div class="section"></div>
+                  <div id="image">
+                    <label for="image"><b>Images</b></label>
                     <div class="file-field input-field">
                       <div class="btn">
-                        <span>File</span>
-                        <input type="file">
+                        <span>Upload image</span>
+                        <input name='image_1' required class='image' type='file' accept="image/*"/>
                       </div>
                       <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Upload one or more files">
+                        <input class="file-path validate" type="text" placeholder="Image here">
                       </div>
                     </div>
-                  </form>
-                  <form action="#">
+                      <!-- <label>Image</label><br>
+                      <input name='image_1' required class='image' type='file' accept="image/*"/><br> -->
+                      <input type="button" class="add_more_image btn amber" value="Add More Image">
+                  </div>
+                  <!--  PDF File : Schedule  -->
+                  <div id="schedule">
+                    <div class="section"></div>
+                    <label for="schedule"><b>Schedule</b></label>
                     <div class="file-field input-field">
                       <div class="btn">
-                        <span>File</span>
-                        <input type="file">
+                        <span>Upload file</span>
+                        <input required name='schedule' required type='file' accept="application/pdf"/>
                       </div>
                       <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Upload one or more files">
+                        <input class="file-path validate" type="text" placeholder="Schedule here">
                       </div>
                     </div>
-                  </form>
-                  <form action="#">
-                    <div class="file-field input-field">
-                      <div class="btn">
-                        <span>File</span>
-                        <input type="file">
-                      </div>
-                      <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Upload one or more files">
-                      </div>
-                    </div>
-                  </form>
-                  <form action="#">
-                    <div class="file-field input-field">
-                      <div class="btn">
-                        <span>File</span>
-                        <input type="file">
-                      </div>
-                      <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Upload one or more files">
-                      </div>
-                    </div>
-                  </form>
-                  <form action="#">
-                    <div class="file-field input-field">
-                      <div class="btn">
-                        <span>File</span>
-                        <input type="file">
-                      </div>
-                      <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" placeholder="Upload one or more files">
-                      </div>
-                    </div>
-                  </form>
+                          <!-- <label>Schedule</label>
+                          <input required name='schedule' type='file' value="" accept="application/pdf"/>
+                          <br> -->
+                  </div>
                 </div>
               </div>
               <div class="row">
@@ -104,6 +82,26 @@
       <?php
       include 'component/footer.php';
       ?>
-
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+      <script>
+            $(document).ready(function(){
+                // add more image
+              $('.add_more_image').click(function(e){
+                e.preventDefault();
+                  var e = document.getElementsByTagName('input');
+                  var i;
+                  var s = 0;
+                  for(i=0; i < e.length; i++) {
+                      if(e[i].type== "file" && e[i].className=="image" ) {
+                          s++;
+                      }
+                  }
+                  if(s < 5){
+                      s++;
+                      $(this).before("<div class='file-field input-field'><div class='btn'><span>Upload image</span><input name='image_" + s + "' class='image' type='file' accept='image/*'/></div><div class='file-path-wrapper'><input class='file-path validate' type='text' placeholder='Image here'></div></div>");
+                  }
+              });
+          });
+          </script>
     </body>
   </html>
