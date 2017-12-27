b2a3009de15e409855ecb37013e46f5cb5a57062
ploychanok <plo_y@live.com>
2017-12-27 13:11:36 +0700

    edit manage ลบหน้าไม่ใช้

--- ChineseTour_NewTheme copy/Manage.php
+++ ChineseTour_NewTheme copy/Manage.php
@@ -13,11 +13,11 @@ include 'module/session.php';
       <div class="section"></div>
       <div class="col s12 l3">
         <div class="collection">
-          <a href="Profile.php" class="collection-item black-text">Profile</a>
+          <!-- <a href="Profile.php" class="collection-item black-text">Profile</a> -->
           <a href="Manage.php" class="collection-item active amber">Manage</a>
           <a href="php_search_tour.php" class="collection-item black-text">Search Tour</a>
-          <a href="Purchase.php" class="collection-item black-text">Purchase</a>
-          <a href="Record.php" class="collection-item black-text">Record</a>
+          <!-- <a href="Purchase.php" class="collection-item black-text">Purchase</a> -->
+          <!-- <a href="Record.php" class="collection-item black-text">Record</a> -->
         </div>
       </div>
       <div class="col s12 l9">
@@ -28,8 +28,6 @@ include 'module/session.php';
           <a href="CreateNews.php" name="createNews" value="Create News" class="btn waves-effect waves-light green darken-1">Create News</a>
         </div>
         <div class="section"></div>
-      </div>
-      <div class="col s12 l9">
         <h5>Tour</h5><hr/>
         <div class="center">
           <a href="php_search_tour.php" name="searchTour" class="btn waves-effect waves-light green darken-1">Search Tour</a>
@@ -37,11 +35,6 @@ include 'module/session.php';
           <a href="AdminCreateTour.php" name="createTour" class="btn waves-effect waves-light green darken-1">Create Tour</a>
         </div>
         <div class="section"></div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col s12 l3"></div>
-      <div class="col s12 l9">
         <h5>About Us Manage</h5><hr/>
         <div class="center">
           <a href="AdminEditAboutUs.php" name="editAboutUs" class="btn waves-effect waves-light green darken-1">Edit AboutUs</a>
