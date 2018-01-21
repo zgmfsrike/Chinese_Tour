d83afdcce4b3972302546f4cc8fe5b5fddd68e9d
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2018-01-21 12:32:57 +0700

    เก็บพวก link page เป็น variable
    เพิ่ม php_change_pass.php เพื่อแยก back กัย front

--- ChineseTour_NewTheme copy/php_create_news.php
+++ ChineseTour_NewTheme copy/php_create_news.php
@@ -2,30 +2,30 @@
 session_cache_expire(30);
 error_reporting (E_ALL ^ E_NOTICE);
 include "db_config.php";
- //include "db_config.php";
+//include "db_config.php";
 include "module/hashing.php";
 //----------------------------Wait for session---------------------------------------------//
 session_start();
 
 if(isset($_SESSION['login_id'])){
-//-----------------------------Create news fucntion----------------------------------------------------//
-if($_POST['save']){
-  $news_topic = $_POST['newsAddtopic'];
-  // $news_image = $_FILES['newsPicAddtopic1'];
-  $news_description = $_POST['newsDescription'];
-  $news_content = $_POST['newsContent'];
-  $start_date = date("Y-m-d");
+  //-----------------------------Create news fucntion----------------------------------------------------//
+  if($_POST['save']){
+    $news_topic = $_POST['newsAddtopic'];
+    // $news_image = $_FILES['newsPicAddtopic1'];
+    $news_description = $_POST['newsDescription'];
+    $news_content = $_POST['newsContent'];
+    $start_date = date("Y-m-d");
 
 
-  //$news_content = $_POST['newsContent'];
+    //$news_content = $_POST['newsContent'];
 
-  if($news_topic != "" && $news_description !=""){
-    $sql= "INSERT INTO news (create_date, topic, short_description,content)
-    VALUES ('$start_date','$news_topic','$news_description','$news_content')";
-    $result = mysqli_query( $GLOBALS['conn'] , $sql );
-    $last_id = mysqli_insert_id($GLOBALS['conn']);
+    if($news_topic != "" && $news_description !=""){
+      $sql= "INSERT INTO news (create_date, topic, short_description,content)
+      VALUES ('$start_date','$news_topic','$news_description','$news_content')";
+      $result = mysqli_query( $GLOBALS['conn'] , $sql );
+      $last_id = mysqli_insert_id($GLOBALS['conn']);
 
-    if ($result) {
+      if ($result) {
         $count_img = 0;
         $count_pdf = 0;
 
@@ -36,85 +36,54 @@ if($_POST['save']){
           }else {
             // echo "Image : ".$i." have file ";
             // echo "<br>";
-              $count_img++;
-              // -----Upload PDF-----
-              $ext = pathinfo(basename($_FILES['newsPicAddtopic'.$i]['name'] ),PATHINFO_EXTENSION);
-              $check_ext = strtolower( $ext);
+            $count_img++;
+            // -----Upload PDF-----
+            $ext = pathinfo(basename($_FILES['newsPicAddtopic'.$i]['name'] ),PATHINFO_EXTENSION);
+            $check_ext = strtolower( $ext);
 
-              if($check_ext == "jpeg" or "jpg" or "png" or "gif"){
+            if($check_ext == "jpeg" or "jpg" or "png" or "gif"){
 
-                $img_path = "images/";
+              $img_path = "images/";
 
-                $img = $_FILES['newsPicAddtopic'.$i]['tmp_name'];
-                $new_image_name = 'img_'.$last_id.'_'.$count_img;
-                $dst = $img_path.$new_image_name ;
+              $img = $_FILES['newsPicAddtopic'.$i]['tmp_name'];
+              $new_image_name = 'img_'.$last_id.'_'.$count_img;
+              $dst = $img_path.$new_image_name ;
 
-                if (($img_info = getimagesize($img)) === FALSE)
-                  die("Image not found or not an image");
+              if (($img_info = getimagesize($img)) === FALSE)
+              die("Image not found or not an image");
 
-                // $width = $img_info[0];
-                // $height = $img_info[1];
-                $width=1280;
-                $height=500;
-                switch ($img_info[2]) {
-                  case IMAGETYPE_GIF  : $src = imagecreatefromgif($img);  break;
-                  case IMAGETYPE_JPEG : $src = imagecreatefromjpeg($img); break;
-                  case IMAGETYPE_PNG  : $src = imagecreatefrompng($img);  break;
-                  default : die("Unknown filetype");
-                }
-                $photoX = ImagesX($src);
-            		$photoY = ImagesY($src);
-
-                $tmp = imagecreatetruecolor($width, $height);
-                imagecopyresampled($tmp, $src, 0, 0, 0, 0, $width, $height, $photoX, $photoY);
-                $success= imagejpeg($tmp, $dst.".jpg");
-                unlink($img_path.$_FILES['newsPicAddtopic'.$i]['name'] );
-
-
-
-
-
-                //
-                // $new_image_name = 'img_'.$last_id.'_'.$i.'.jpg';
-                // $img_path = "images/";
-                // $upload_path = $img_path.$new_image_name;
-                //
-                // $images = $_FILES['newsPicAddtopic'.$i]["tmp_name"];
-            		// copy($_FILES['newsPicAddtopic'.$i]["tmp_name"],"images/".$_FILES['newsPicAddtopic'.$i]["name"]);
-            		// $width=100; //*** Fix Width & Heigh (Autu caculate) ***//
-            		// $size=GetimageSize($images);
-            		// $height=round($width*$size[1]/$size[0]);
-                // if($ext=="jpeg" or "jpg"){
-                //     $images_orig = ImageCreateFromJPEG($images);
-                // }else if($ext =="png"){
-                //     $images_orig = imagecreatefrompng($images);
-                // }else if($ext =="gif"){
-                //     $images_orig = imagecreatefromgif($images);
-                // }
-                //
-            		// $photoX = ImagesX($images_orig);
-            		// $photoY = ImagesY($images_orig);
-            		// $images_fin = ImageCreateTrueColor($width, $height);
-            		// ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
-            		// $success= ImageJPEG($images_fin,"images/".  $new_image_name);
-            		// ImageDestroy($images_orig);
-            		// ImageDestroy($images_fin);
-
-
-
-                // $success = move_uploaded_file($_FILES['newsPicAddtopic'.$i]['tmp_name'] ,$upload_path);
-                if($success == FALSE){
-                  echo "Cannot upload images";
-                  exit();
-                }
-                $news_image = $new_image_name.".jpg";
-
-                // ---------------------------
-                $sql2 = "INSERT INTO news_image(news_id, news_image,img_index) VALUES ('$last_id','$news_image','$count_img')";
-                $result2 = mysqli_query( $GLOBALS['conn'] , $sql2 );
-
-
+              // $width = $img_info[0];
+              // $height = $img_info[1];
+              $width=1280;
+              $height=500;
+              switch ($img_info[2]) {
+                case IMAGETYPE_GIF  : $src = imagecreatefromgif($img);  break;
+                case IMAGETYPE_JPEG : $src = imagecreatefromjpeg($img); break;
+                case IMAGETYPE_PNG  : $src = imagecreatefrompng($img);  break;
+                default : die("Unknown filetype");
               }
+              $photoX = ImagesX($src);
+              $photoY = ImagesY($src);
+
+              $tmp = imagecreatetruecolor($width, $height);
+              imagecopyresampled($tmp, $src, 0, 0, 0, 0, $width, $height, $photoX, $photoY);
+              $success= imagejpeg($tmp, $dst.".jpg");
+              unlink($img_path.$_FILES['newsPicAddtopic'.$i]['name'] );
+
+
+              // $success = move_uploaded_file($_FILES['newsPicAddtopic'.$i]['tmp_name'] ,$upload_path);
+              if($success == FALSE){
+                echo "Cannot upload images";
+                exit();
+              }
+              $news_image = $new_image_name.".jpg";
+
+              // ---------------------------
+              $sql2 = "INSERT INTO news_image(news_id, news_image,img_index) VALUES ('$last_id','$news_image','$count_img')";
+              $result2 = mysqli_query( $GLOBALS['conn'] , $sql2 );
+
+
+            }
 
           }
 
@@ -137,19 +106,19 @@ if($_POST['save']){
             // $add_name = mysqli_query($GLOBALS['conn'] , $sql_add_pdf_name);
 
 
-              $new_pdf_name = 'pdf_'.$last_id.'_'.$count_pdf.'.'.$ext;
-              $pdf_path = "pdf/";
-              $upload_path_pdf = $pdf_path.$new_pdf_name;
-              $success = move_uploaded_file($_FILES['newsPdf'.$j]['tmp_name'] ,$upload_path_pdf);
-              if($success == FALSE){
-                echo "Cannot upload pdf";
-                exit();
-              }
-              $news_pdf = $new_pdf_name;
+            $new_pdf_name = 'pdf_'.$last_id.'_'.$count_pdf.'.'.$ext;
+            $pdf_path = "pdf/";
+            $upload_path_pdf = $pdf_path.$new_pdf_name;
+            $success = move_uploaded_file($_FILES['newsPdf'.$j]['tmp_name'] ,$upload_path_pdf);
+            if($success == FALSE){
+              echo "Cannot upload pdf";
+              exit();
+            }
+            $news_pdf = $new_pdf_name;
 
-              // ---------------------------
-              $sql3 = "INSERT INTO news_pdf(news_id, news_pdf,pdf_name,pdf_index) VALUES ('$last_id','$news_pdf','$old_pdf_name','$count_pdf')";
-              $result3 = mysqli_query( $GLOBALS['conn'] , $sql3 );
+            // ---------------------------
+            $sql3 = "INSERT INTO news_pdf(news_id, news_pdf,pdf_name,pdf_index) VALUES ('$last_id','$news_pdf','$old_pdf_name','$count_pdf')";
+            $result3 = mysqli_query( $GLOBALS['conn'] , $sql3 );
 
 
 
@@ -165,18 +134,18 @@ if($_POST['save']){
 
         header("location: message.php?msg=create_news_succ");
 
-  }
+      }
 
 
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-// -------------------------
-
-
-
-
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
+    // -------------------------
+
+
+
+
+  }
 
 
 
--- ChineseTour_NewTheme copy/php_edit_func.php
+++ ChineseTour_NewTheme copy/php_edit_func.php
@@ -15,45 +15,45 @@ session_start();
 if(isset($_SESSION['login_id'])){
   $id = $_SESSION['login_id'];
   //---------------------- DB Value----------------------
-$sql_db =  "SELECT * FROM `member` WHERE id=$id" ;
-$result2 = mysqli_query($conn,$sql_db);
-$data = mysqli_fetch_array($result2);
-$firstname_db = $data['first_name'];
-$middlename_db = $data['middle_name'];
-$lastname_db = $data['last_name'];
-$phone_db = $data['phone'];
-$email_db = $data['email'];
-$salary_db = $data['salary'];
-$occupation_db = $data['occupation'];
-$date_of_birth = $data['dob'];
-$address_db = $data['address'];
-$city_db = $data['city'];
-$province_db = $data['province'];
-$zipcode_db = $data['zipcode'];
+  $sql_db =  "SELECT * FROM `member` WHERE id=$id" ;
+  $result2 = mysqli_query($conn,$sql_db);
+  $data = mysqli_fetch_array($result2);
+  $firstname_db = $data['first_name'];
+  $middlename_db = $data['middle_name'];
+  $lastname_db = $data['last_name'];
+  $phone_db = $data['phone'];
+  $email_db = $data['email'];
+  $salary_db = $data['salary'];
+  $occupation_db = $data['occupation'];
+  $date_of_birth = $data['dob'];
+  $address_db = $data['address'];
+  $city_db = $data['city'];
+  $province_db = $data['province'];
+  $zipcode_db = $data['zipcode'];
 
-//----------------------------------POST Value------------------------
+  //----------------------------------POST Value------------------------
 
-$firstname = $_POST['firstname'];
-$middlename = $_POST['middlename'];
-$surname = $_POST['lastname'];
-$occupation =  $_POST['occupation'];
-$salary = $_POST['salary'];
-$dob = date('Y-m-d',strtotime($_POST['dob']));
-$phone = $_POST['phone'];
+  $firstname = $_POST['firstname'];
+  $middlename = $_POST['middlename'];
+  $surname = $_POST['lastname'];
+  $occupation =  $_POST['occupation'];
+  $salary = $_POST['salary'];
+  $dob = date('Y-m-d',strtotime($_POST['dob']));
+  $phone = $_POST['phone'];
 
 
-$address = $_POST['address'];
-$city = $_POST['city'];
-$province = $_POST['province'];
-$zipcode = $_POST['zipcode'];
-$country_code = $_POST['countrycode'];
-//-----------------------------Edit fucntion----------------------------------------------------//
-if($_POST['save']){
+  $address = $_POST['address'];
+  $city = $_POST['city'];
+  $province = $_POST['province'];
+  $zipcode = $_POST['zipcode'];
+  $country_code = $_POST['countrycode'];
+  //-----------------------------Edit fucntion----------------------------------------------------//
+  if($_POST['save']){
 
     $sql= "UPDATE `member` SET `first_name`='$firstname',`middle_name`='$middlename',`last_name`='$surname',
-                                `phone`='$phone',`occupation`='$occupation',`salary`='$salary',`dob`='$dob',`address`='$address',`city`='$city',`province`='$province',`zipcode`='$zipcode',`country_code`='$country_code'
-
-                                 WHERE id = $id ";
+    `phone`='$phone',`occupation`='$occupation',`salary`='$salary',`dob`='$dob',`address`='$address',`city`='$city',`province`='$province',`zipcode`='$zipcode',`country_code`='$country_code'
+    
+    WHERE id = $id ";
     $result = mysqli_query( $GLOBALS['conn'] , $sql );
     if($result){
       header("location: message.php?msg=edit");
@@ -62,5 +62,5 @@ if($_POST['save']){
 
 
 
-}
+  }
 }
