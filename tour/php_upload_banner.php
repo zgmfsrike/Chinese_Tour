5ff85b4d1f04b7d2d56268d272e2e9bc76c62644
ploychanok <plo_y@live.com>
2018-02-22 23:12:13 +0700

    no limit pic size

--- tour/php_upload_banner.php
+++ tour/php_upload_banner.php
@@ -61,11 +61,7 @@ include "db_config.php";
       //   }
       // }
 
-        // Check file size
-        if ($_FILES["$temp"]["size"] > 500000) {
-          echo "Sorry, your file is too large.";
-          $uploadOk = 0;
-        }
+        
         // Allow certain file formats
         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
           echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
