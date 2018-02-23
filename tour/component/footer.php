abb2b468496b1990d7436c762b1f8c101e108b59
ploychanok <plo_y@live.com>
2018-02-24 00:19:33 +0700

    WeChat

--- tour/component/footer.php
+++ tour/component/footer.php
@@ -8,7 +8,7 @@
        <div class="col l4 offset-l2 s12">
          <h5 class="white-text">Contact Us</h5>
          <ul>
-           <li>WeChat : 591198421 or HTCE888</li>
+           <li><a class="modal-trigger white-text" href="#modal1">WeChat : 591198421 or HTCE888</a></li>
            <li>Tel : 081-025-0351</li>
            <li><a class="grey-text text-lighten-3" href="mailto:chiangmaihongthai@hotmail.com">Email : chiangmaihongthai@hotmail.com</a></li>
          </ul>
@@ -20,6 +20,38 @@
      © 2017 Copyright CAMT
      </div>
    </div>
+
+   <!-- Modal Trigger -->
+
+
+<!-- Modal Structure -->
+<div id="modal1" class="modal">
+  <div class="modal-content">
+    <div class="row">
+      <div class="col s7 hide-on-med-and-down">
+        <h5 class="black-text center"><b>WeChat QR Code</b></h5>
+        <img width="100%" src="images/wecharQR.jpg">
+      </div>
+
+      <div class="col s12 hide-on-large-only">
+        <h5 class="black-text center"><b>WeChat QR Code</b></h5>
+        <img width="100%" src="images/wecharQR.jpg">
+      </div>
+
+      <div class="col s5 black-text hide-on-med-and-down">
+        <h5 class="center">Contact Us</h5></br>
+        <p><b>Chiangmai Hongthai</b></p>
+        <p><b>Address</b></br>
+          Sridonchai Road Phrasingha District Aumphoe Mueang
+          Chiangmai </br>Thailand</p>
+        <p><b>WeChat</b></br>591198421 or HTCE888</p>
+        <p><b>Telephone</b></br>081-025-0351</p>
+        <p><b>Email</b></br>chiangmaihongthai@hotmail.com</p>
+      </div>
+    </div>
+
+  </div>
+</div>
 
    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
    <script src="js/materialize.min.js"></script>
