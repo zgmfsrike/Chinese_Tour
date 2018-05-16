ec2c46ba708747cd02db2099f139b99cdc9ba292
nira <nayzaa2010@hotmail.com>
2018-05-16 14:30:36 +0700

    add edit feedback page

--- tour/message.php
+++ tour/message.php
@@ -47,7 +47,7 @@ require 'module/language/init.php';
   if(isset($_GET['msg'])){
     $msg = $_GET['msg'];
 
-    // add message -> $message = message(HEADER,message,LINK,BUTTON_VALUE);
+    // add message -> $result = message(HEADER,message,LINK,BUTTON_VALUE);
     // use '' in LINK or BUTTON_VALUE to use DEFAULT as back button
 
     require 'module/message/session.php';
@@ -58,75 +58,78 @@ require 'module/language/init.php';
 
     // manage_account.php
 
-    // AUTHENTICATIO
+    // AUTHENTICATION
     if($msg == 'create_news_succ')
     if(isset($_GET['id'])){
-      $message = message('Success!','News has been created ','news.php?news_id='.$_GET['id'],'back to news page');
+      $result = message('Success!','News has been created ','news.php?news_id='.$_GET['id'],'back to news page');
     }else{
-      $message = message('Request not found','','','');
+      $result = message('Request not found','','','');
     }
 
-    if($msg == 'del_news_succ') $message = message('Success!','News has been deleted ','index.php','Go to home page');
+    if($msg == 'del_news_succ') $result = message('Success!','News has been deleted ','index.php','Go to home page');
 
 
     if($msg == 'edit_news_succ')
     if(isset($_GET['id'])){
-      $message = message('Success!','News has been updated.','news.php?news_id='.$_GET['id'],'back to news page');
+      $result = message('Success!','News has been updated.','news.php?news_id='.$_GET['id'],'back to news page');
     }else {
-      $message = message('Request not found','','','');
+      $result = message('Request not found','','','');
     }
 
-    if($msg == 'not_image') $message = message('Sorry!','Only jpg, gif, and png files are allowed. ','index.php','Go to home page');
+    if($msg == 'not_image') $result = message('Sorry!','Only jpg, gif, and png files are allowed. ','index.php','Go to home page');
 
 
-    if($msg == 'not_pdf') $message = message('Sorry!','Only pdf file are allowed. ','index.php','Go to home page');
+    if($msg == 'not_pdf') $result = message('Sorry!','Only pdf file are allowed. ','index.php','Go to home page');
 
 
     //Index manage
-    if($msg == 'uploadSucc') $message = message('Got it!',' The file has been uploaded.','index.php','Go to home page');
+    if($msg == 'uploadSucc') $result = message('Got it!',' The file has been uploaded.','index.php','Go to home page');
 
-    if($msg == 'uploadNotSucc') $message = message('Sorry, your file was not uploaded.','Please check neither your file is too large, nor your file type is wrong (Only JPG, JPEG, PNG & GIF files are allowed).','index.php','Go to home page');
+    if($msg == 'uploadNotSucc') $result = message('Sorry, your file was not uploaded.','Please check neither your file is too large, nor your file type is wrong (Only JPG, JPEG, PNG & GIF files are allowed).','index.php','Go to home page');
 
 
     // TOUR
     if($msg == 'create_tour_succ')
     if(isset($_GET['id'])){
-      $message = message('Success!','Create tour successfully','tour.php?id='.$_GET['id'],'Back to tour page');
+      $result = message('Success!','Create tour successfully','tour.php?id='.$_GET['id'],'Back to tour page');
     }else{
-      $message = message('Request not found','','','');
+      $result = message('Request not found','','','');
     }
 
-    if($msg == 'delete_tour_succ')$message = message('Success!','Deleting tour successfully','index.php','Go to home page');
+    if($msg == 'delete_tour_succ') $result = message('Success!','Deleting tour successfully','index.php','Go to home page');
 
     if($msg == 'edit_tour_succ')
     if(isset($_GET['id'])){
-      $message = message('Success!','Editing tour successfully','tour.php?id='.$_GET['id'],'Back to tour page');
+      $result = message('Success!','Editing tour successfully','tour.php?id='.$_GET['id'],'Back to tour page');
     }else{
-      $message = message('Request not found','','','');
+      $result = message('Request not found','','','');
     }
 
-    if($msg == 'tour_not_found')$message = message('Sorry!','Tour not found.','index.php','Go to home page');
+    if($msg == 'tour_not_found')$result = message('Sorry!','Tour not found.','index.php','Go to home page');
 
 
     // request
-    if($msg == 'unknow_request') $message = message('Unknow request','Something went wrong, please try again.','','');
+    if($msg == 'unknow_request') $result = message('Unknow request','Something went wrong, please try again.','','');
 
-    if($msg == 'error') $message = message('Something went wrong','Please try again.','','');
+    if($msg == 'error') $result = message('Something went wrong','Please try again.','','');
 
 
     // Announce
-    if($msg == 'success_update_announce') $message = message('Complete','Update announce complete','index.php','Go to home page');
+    if($msg == 'success_update_announce') $result = message('Complete','Update announce complete','index.php','Go to home page');
 
     // under construction
-    if($msg == 'under_construction') $message = message('This page is under construction','Sorry for inconvenient','index.php','Go to home page');
+    if($msg == 'under_construction') $result = message('This page is under construction','Sorry for inconvenient','index.php','Go to home page');
 
-    if($msg == 'book_tour_succ')  message('Success!','Send e-mail to member complete','index.php','Go to home page');
+    if($msg == 'book_tour_succ') $result =  message('Success!','Send e-mail to member complete','index.php','Go to home page');
+
+    // feedback
+    if($msg == 'edit_feedback_question_complete') $result =  message('Success!','Edit feedback form successfully.','','');
 
 
 
 
     // default
-    if(!isset($message)){
+    if(!isset($result)){
       message('Request not found','','','');
     }
   }else{
