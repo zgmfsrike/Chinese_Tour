95345d8717522f951bfb73a424b91db190473042
ploychanok <plo_y@live.com>
2018-05-22 17:20:24 +0700

    update feedback questionnaire style

--- tour/edit_feedback_form.php
+++ tour/edit_feedback_form.php
@@ -1,6 +1,8 @@
 <?php
 include 'module/session.php';
 include 'db_config.php';
+require 'module/language/init.php';
+require 'module/language/lang_index.php';
 
 // $edit_feedback_question = "php_edit_feedback_question.php";
 $edit_feedback_question = "php_edit_feedback_form.php";
@@ -9,30 +11,46 @@ $sql = "SELECT * FROM feedback_question WHERE version = (SELECT MAX(version) FRO
 $result = mysqli_query($conn,$sql);
 
  ?>
-
-
+<!DOCTYPE html>
 <html>
+<?php
+$title = "Chiang Mai Hong Thai Tour";
+include 'component/header.php';
+?>
+<body>
 
-<form action="<?php echo $edit_feedback_question; ?>" method="post">
-  <?php
-  for($i = 1; $i <= 15; $i++){
+<div class="container col s12">
+  <br/>
+  <h3 class="center"><b>Feedback Questionnaire</b></h3>
+  <form action="<?php echo $edit_feedback_question; ?>" method="post">
+    <?php
+    for($i = 1; $i <= 15; $i++){
 
-    $row = mysqli_fetch_array($result);
+      $row = mysqli_fetch_array($result);
 
-    $question = $row['question'];
-    $enable = $row['enable'];
+      $question = $row['question'];
+      $enable = $row['enable'];
 
-    if($enable == 1){
-      $enable = "checked";
-    }else{
-      $enable = "";
+      if($enable == 1){
+        $enable = "checked";
+      }else{
+        $enable = "";
+      }
+
+
+      echo 'Q'.$i.': <input type="text" name="question_'.$i.'" value="'.$question.'"/> enable: <input type="checkbox" name="enable_'.$i.'" '.$enable.'/> <br>';
     }
+     ?>
+  <div class="center-align" style="margin-bottom:30px; margin-top:30px;">
+    <input class="btn green" type="submit" name="save" value="Save">
+  </div>
+  </form>
+</div>
 
+  <!--Footer-->
+  <?php
+  include 'component/footer.php';
+  ?>
 
-    echo 'Q'.$i.': <input type="text" name="question_'.$i.'" value="'.$question.'"/> enable: <input type="checkbox" name="enable_'.$i.'" '.$enable.'/> <br>';
-  }
-   ?>
-<input type="submit" name="save" value="Save">
-
-</form>
+</body>
 </html>
