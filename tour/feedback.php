5bf23dbc35f4eb3fbaae500a2f2d054230fe4e77
ploychanok <plo_y@live.com>
2018-05-22 17:07:37 +0700

    update feedback style

--- tour/feedback.php
+++ tour/feedback.php
@@ -16,28 +16,30 @@ $link = "php_feedback.php";
 $title = "Feedback Form";
 // include 'component/header.php';
 ?>
+<?php
+$title = "Chiang Mai Hong Thai Tour";
+include 'component/header.php';
+?>
 <body>
-  <div class="container">
+  <div class="container col s12">
     <form action=<?php echo $link;?> method="POST">
-      <h1>Feedback Form</h1>
+      <br/>
+      <h3 class="center"><b>Feedback Form</b></h3>
       <div>
         <table class="responsive-table">
           <thead>
             <tr>
-              <th>Question</th>
-              <th>Very Satisfied</th>
-              <th>Satisfied</th>
-              <th>Neutral</th>
-              <th>Unsatisfied</th>
-              <th>Very Unsatisfied</th>
-
+              <th class="center-align">Question</th>
+              <th class="center-align">Very Satisfied</th>
+              <th class="center-align">Satisfied</th>
+              <th class="center-align">Neutral</th>
+              <th class="center-align">Unsatisfied</th>
+              <th class="center-align">Very Unsatisfied</th>
             </tr>
           </thead>
+
           <tbody>
             <?php
-
-
-
             $sql = "SELECT * FROM feedback_question WHERE version = (SELECT MAX(version) FROM feedback_question) AND enable=1";
             $result = mysqli_query($conn, $sql);
             if($result){
@@ -77,21 +79,21 @@ $title = "Feedback Form";
             ?>
 
           </tbody>
-
-
-
         </table>
       </div>
-      <input type="text" name="feedback_id" id='feedback_id' value="<?php if($feedback_id !==""){ echo $feedback_id;}?>" style="display:none" >
-      <lable for="comment">Comment</lable>
-      <textarea id="comment" name="comment"></textarea>
+
+        <input type="text" name="feedback_id" id='feedback_id' value="<?php if($feedback_id !==""){ echo $feedback_id;}?>" style="display:none" >
+        <br/>
+
+        <div style="margin-right:30px; margin-left:10px;">
+          <lable for="comment" class="hide-on-med-and-down show-on-large">Comment</lable>
+          <textarea placeholder="Comment Here" id="comment" name="comment" class="materialize-textarea" style="overflow-y: scroll; border: 1px solid;padding: 10px; margin-top:10px;"></textarea>
+        </div>
+
       <div class="right-align">
         <input class="btn green " type="submit" name="submit" value="Confirm"><br />
+        <br/>
       </div>
-      <p>
-
-      </p>
-
 
     </form>
   </div>
@@ -99,9 +101,8 @@ $title = "Feedback Form";
 </body>
 
 <!--Footer-->
-
 <?php
-// include 'component/footer.php';
+include 'component/footer.php';
 ?>
 
 
