e6c96a8dbb412c495b790fc0dd111f598277ca40
nira <nayzaa2010@hotmail.com>
2018-06-07 17:28:48 +0700

    edit about us

--- tour/admin_edit_about_us.php
+++ tour/admin_edit_about_us.php
@@ -2,77 +2,69 @@
 include 'db_config.php';
 include('module/session.php');
 if(!isLoginAs(array('admin'))){
-    header('Location: message.php?msg=unauthorized');
+  header('Location: message.php?msg=unauthorized');
 }
 
 require 'module/language/init.php';
 ?>
 <!DOCTYPE html>
 <html>
-    <?php
-    $title = "Edit About Us";
-    include 'component/header.php';
-    ?>
-    <body>
+<?php
+$title = "Edit About Us";
+include 'component/header.php';
 
-        <!--Edit News Here-->
+$sql = "SELECT * FROM `page_about` WHERE name = 'about_content'";
+$result = mysqli_query($conn, $sql);
+if(mysqli_num_rows($result) == 0){
+  header("location: message.php");
+}
+$data = mysqli_fetch_array($result);
+$about_en = $data['english'];
+$about_ch = $data['chinese'];
+$about_th = $data['thai'];
+?>
+<body>
 
-        <div class="container">
-            <h3>Edit About Us</h3>
-            <div class="row">
-                <div class="col s12">
-                    <h5>Say something! (section 1)</h5>
-                    <div class="input-field col s12">
-                        <textarea id="textarea1" class="materialize-textarea"></textarea>
-                        <label for="textarea1">Textarea</label>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12">
-                    <h5>Say something! (section 2)</h5>
-                    <div class="input-field col s12">
-                        <textarea id="textarea1" class="materialize-textarea"></textarea>
-                        <label for="textarea1">Textarea</label>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12 l6">
-                    <h5>Change Image</h5>
-                    <form action="#">
-                        <div class="file-field input-field">
-                            <div class="btn">
-                                <span>File</span>
-                                <input type="file">
-                            </div>
-                            <div class="file-path-wrapper">
-                                <input class="file-path validate" type="text" placeholder="Upload file">
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s12">
-                    <h5>Change Contact Us</h5>
-                    <div class="input-field col s12">
-                        <textarea id="textarea1" class="materialize-textarea"></textarea>
-                        <label for="textarea1">Textarea</label>
-                    </div>
-                </div>
-            </div>
+  <!--Edit News Here-->
+
+  <div class="container">
+    <form class="col s12" method="post" name="announce" action="php_edit_aboutus.php">
+      <h3>Edit About Us</h3>
+      <div class="row">
+        <div class="col s12">
+          <p>English</p>
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea" name="about_en"><?php echo $about_en;?></textarea>
+            <label for="textarea1">Enlish</label>
+          </div>
         </div>
-        <div class="row col s12 center">
-            <button name="Submit" type="submit" value="Cancel" onclick="window.location.href='manage.php'" class="waves-effect waves-light btn red">Cancel</button>
-            <button type="submit" class="waves-effect waves-light btn green" value="Save">Save</button>
+        <div class="col s12">
+          <p>Chinese</p>
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea" name="about_ch"><?php echo $about_ch;?></textarea>
+            <label for="textarea1">Chinese</label>
+          </div>
         </div>
+        <div class="col s12">
+          <p>Thai</p>
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea" name="about_th"><?php echo $about_th;?></textarea>
+            <label for="textarea1">Thai</label>
+          </div>
         </div>
+      </div>
+    </div>
+    <div class="row col s12 center">
+      <button name="Submit" type="submit" value="Cancel" onclick="window.location.href='manage.php'" class="waves-effect waves-light btn red">Cancel</button>
+      <button type="submit" class="waves-effect waves-light btn green" value="save" name="save">Save</button>
+    </div>
+  </form>
+</div>
 
-    <!--Footer-->
-    <?php
-    include 'component/footer.php';
-    ?>
+<!--Footer-->
+<?php
+include 'component/footer.php';
+?>
 
-    </body>
+</body>
 </html>
