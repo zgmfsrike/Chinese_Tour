449879890018d061280f77e5596e16abd4109e96
ploychanok <plo_y@live.com>
2018-07-17 18:49:17 +0700

    fix index

--- tour/index.php
+++ tour/index.php
@@ -53,8 +53,6 @@ include 'component/header.php';
         ?>
       </ul>
     </div>
-
-
 <!--Notice-->
 <div class="container row">
   <div class="col s12 m12">
@@ -62,8 +60,8 @@ include 'component/header.php';
       <div class="col s12 m12">
         <div class="card orange lighten-1">
           <div class="card-content white-text">
-            <span class="card-title"><?php echo $string_index_announcement;?> <i class="material-icons">announcement</i></span>
-            <blockquote>
+            <span class="card-title"> <h4><?php echo $string_index_announcement;?> <i class="material-icons">announcement</i></h4> </span>
+            <blockquote style="font-size:130%;">
               <?php echo  $string_index_announcement_cont; ?>
             </blockquote>
           </div>
@@ -71,48 +69,45 @@ include 'component/header.php';
       </div>
     </div>
 
-
-
 <!--News-->
-  <!-- ก๊อปเกิน????-->
-  <h3><?php echo $string_index_news;?></h3>
-  <?php
-  $sql= "SELECT n.news_id,n.topic,n.short_description FROM news_".$_COOKIE['lang']." n ORDER BY n.news_id DESC limit 3";
-  $result = mysqli_query( $GLOBALS['conn'] , $sql );
-  $img_path = "./images/";
+  <div class="container row col s12">
+    <h3><?php echo $string_index_news;?></h3>
+    <?php
+    $sql= "SELECT n.news_id,n.topic,n.short_description FROM news_".$_COOKIE['lang']." n ORDER BY n.news_id DESC limit 3";
+    $result = mysqli_query( $GLOBALS['conn'] , $sql );
+    $img_path = "./images/";
 
-  while($show = mysqli_fetch_array($result)) {
-    $news_id = $show['news_id'];
-    $sql_img = "SELECT n.news_id,ni.news_image FROM news_".$_COOKIE['lang']." n INNER JOIN news_image ni on n.news_id = ni.news_id
-    WHERE n.news_id = '$news_id'";
-    $result_img = mysqli_query( $GLOBALS['conn'] , $sql_img );
-    $show_img = mysqli_fetch_array($result_img);
-    $img_name = $show_img['news_image'];
-    if(!$show_img){
-      $img_name = "No_Image_Available.png";
+    while($show = mysqli_fetch_array($result)) {
+      $news_id = $show['news_id'];
+      $sql_img = "SELECT n.news_id,ni.news_image FROM news_".$_COOKIE['lang']." n INNER JOIN news_image ni on n.news_id = ni.news_id
+      WHERE n.news_id = '$news_id'";
+      $result_img = mysqli_query( $GLOBALS['conn'] , $sql_img );
+      $show_img = mysqli_fetch_array($result_img);
+      $img_name = $show_img['news_image'];
+      if(!$show_img){
+        $img_name = "No_Image_Available.png";
+      }
+      $img_file = $img_path.$img_name."?".$time;
+      echo "<div class='col s12 m4'>
+      <div class='card small'>
+      <div class='card-image'>
+
+      <img src='$img_file'  height='263' width='370'>
+      <span class='card-title' style='white-space: nowrap;width: 12em;overflow: hidden;text-overflow: ellipsis;'>".$show['topic']."</span>
+      </div>
+      <div class='card-content'>
+      <p style='white-space: nowrap;width: 17em;overflow: hidden;text-overflow: ellipsis;'>".$show['short_description']."</p>
+      </div>
+      <div class='card-action'>
+      <a href='news.php?news_id=$news_id'>"."$string_index_readmore"."</a>
+      </div>
+      </div>
+      </div>";
+
     }
-    $img_file = $img_path.$img_name."?".$time;
-    echo "<div class='col s12 m4'>
-    <div class='card small'>
-    <div class='card-image'>
-
-    <img src='$img_file'  height='263' width='370'>
-    <span class='card-title' style='white-space: nowrap;width: 12em;overflow: hidden;text-overflow: ellipsis;'>".$show['topic']."</span>
-    </div>
-    <div class='card-content'>
-    <p style='white-space: nowrap;width: 17em;overflow: hidden;text-overflow: ellipsis;'>".$show['short_description']."</p>
-    </div>
-    <div class='card-action'>
-    <a href='news.php?news_id=$news_id'>"."$string_index_readmore"."</a>
-    </div>
-    </div>
-    </div>";
-
-  }
-  ?>
-
+    ?>
+  </div>
   <a href='<?php echo $link_all_news; ?>'><button type='button' class='btn ' name='button'>View All News</button></a>
-  <!-- ^ ก๊อปเกิน???? ^-->
 
     <h3><?php echo $string_index_tour;?></h3>
 
