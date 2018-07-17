17ee9218bf7b4362b2bd043b646d50c63faaa84a
ploychanok <plo_y@live.com>
2018-07-17 17:38:04 +0700

    fix container index

--- tour/index.php
+++ tour/index.php
@@ -54,7 +54,6 @@ include 'component/header.php';
       </ul>
     </div>
 
-
 <!--Notice-->
 <div class="container row">
   <div class="col s12 m12">
@@ -71,48 +70,8 @@ include 'component/header.php';
       </div>
     </div>
 
-    <!--News-->
-      <h3><?php echo $string_index_news;?></h3>
-      <?php
-      $sql= "SELECT n.news_id,n.topic,n.short_description FROM news_".$_COOKIE['lang']." n ORDER BY n.news_id DESC limit 3";
-      $result = mysqli_query( $GLOBALS['conn'] , $sql );
-      $img_path = "./images/";
-
-      while($show = mysqli_fetch_array($result)) {
-        $news_id = $show['news_id'];
-        $sql_img = "SELECT n.news_id,ni.news_image FROM news_".$_COOKIE['lang']." n INNER JOIN news_image ni on n.news_id = ni.news_id
-        WHERE n.news_id = '$news_id'";
-        $result_img = mysqli_query( $GLOBALS['conn'] , $sql_img );
-        $show_img = mysqli_fetch_array($result_img);
-        $img_name = $show_img['news_image'];
-        if(!$show_img){
-          $img_name = "No_Image_Available.png";
-        }
-        $img_file = $img_path.$img_name;
-        echo "<div class='col s12 m4'>
-        <div class='card small'>
-        <div class='card-image'>
-
-        <img src='$img_file'  height='263' width='370'>
-        <span class='card-title' style='white-space: nowrap;width: 12em;overflow: hidden;text-overflow: ellipsis;'>".$show['topic']."</span>
-        </div>
-        <div class='card-content'>
-        <p style='white-space: nowrap;width: 17em;overflow: hidden;text-overflow: ellipsis;'>".$show['short_description']."</p>
-        </div>
-        <div class='card-action'>
-        <a href='news.php?news_id=$news_id'>"."$string_index_readmore"."</a>
-        </div>
-        </div>
-        </div>";
-
-      }
-      ?>
-      <div class="right">
-        <a href='<?php echo $link_all_news; ?>'><button type='button' class='btn' name='button'>View All News</button></a>
-      </div>
-
-<!--News-->
   <!-- ก๊อปเกิน????-->
+  <div class="container row">
   <h3><?php echo $string_index_news;?></h3>
   <?php
   $sql= "SELECT n.news_id,n.topic,n.short_description FROM news_".$_COOKIE['lang']." n ORDER BY n.news_id DESC limit 3";
@@ -148,8 +107,9 @@ include 'component/header.php';
 
   }
   ?>
+</div>
+  <a href='<?php echo $link_all_news; ?>'><button type='button' class='btn' name='button'>View All News</button></a>
 
-  <a href='<?php echo $link_all_news; ?>'><button type='button' class='btn ' name='button'>View All News</button></a>
   <!-- ^ ก๊อปเกิน???? ^-->
 
     <h3><?php echo $string_index_tour;?></h3>
